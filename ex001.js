0c04a2becbc4bb6ee4b62a14238b718d146b92de
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-12 19:08:13 -0300

    Update ex001.js

--- ex001.js
+++ ex001.js
@@ -1,2 +1,12 @@
-const resultado = (nota) => nota >= 7? "Aprovado" : "Reprovado";
-console.log(resultado(6))
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor);
+}
+
+let opcao = 0;
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10);
+    console.log(`Opção escolhida foi ${opcao}`);
+}
+console.log('Até a próxima');
