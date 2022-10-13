e8f3028afd734440d4140cc1530109b00164a60e
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-13 15:32:59 -0300

    Update ex002.js

--- ex002.js
+++ ex002.js
@@ -1,48 +1,57 @@
-// Criação de funções
+// // Criação de funções
 
-// 1) Forma literal
-function acao() {
+// // 1) Forma literal
+// function acao() {
 
+// }
+
+// // 2) Armazenr em uma variável
+// const acao2 = function() {
+
+// }
+
+// // 3) Armazenar em Array
+// const array1 =  [function(a, b) {
+//     return a + b;
+// }, acao, acao2]
+// console.log(array1[0](2, 3))
+
+// // 4) Armazenar em um atributo de objeto
+// const obj = {}
+// obj.falar = function () {
+//     return 'Opa';
+// }
+// console.log(obj.falar())
+
+// // 5) Passar funcção como parâmetro de outra função
+// function acao3(acao) {
+//     acao()
+// }
+// acao3( function() {
+//     console.log('Oláaaa')
+// })
+
+// // 6) Adicionando parâmetros à função
+// function soma() {
+//     let soma = 0
+//     for (i in arguments) {
+//         soma += arguments[i]
+//     }
+//     return soma
+// }
+// console.log(soma(1))
+// console.log(soma(1, 2))
+// console.log(soma(1, 2, 3))
+// console.log(soma(1, 2, 3, 4))
+
+function pessoa() {
+    let idade = 0;
+
+    setInterval(() => {
+        console.log(idade);
+        idade++;
+        
+    }, 2000);
 }
 
-// 2) Armazenr em uma variável
-const acao2 = function() {
-
-}
-
-// 3) Armazenar em Array
-const array1 =  [function(a, b) {
-    return a + b;
-}, acao, acao2]
-console.log(array1[0](2, 3))
-
-// 4) Armazenar em um atributo de objeto
-const obj = {}
-obj.falar = function () {
-    return 'Opa';
-}
-console.log(obj.falar())
-
-// 5) Passar funcção como parâmetro de outra função
-function acao3(acao) {
-    acao()
-}
-acao3( function() {
-    console.log('Oláaaa')
-})
-
-// 6) Adicionando parâmetros à função
-function soma() {
-    let soma = 0
-    for (i in arguments) {
-        soma += arguments[i]
-    }
-    return soma
-}
-console.log(soma(1))
-console.log(soma(1, 2))
-console.log(soma(1, 2, 3))
-console.log(soma(1, 2, 3, 4))
-
-// 7) Forma atual de adicionar parâmetro
-
+new pessoa;
