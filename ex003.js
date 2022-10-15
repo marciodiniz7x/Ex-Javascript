3964b71d82223154804a3a94812a229329d274c7
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-15 14:12:23 -0300

    Update ex003.js

--- ex003.js
+++ ex003.js
@@ -1,10 +1,33 @@
-const fabricantes = ['Mercedes', 'Audi', 'BMW'];
+// const fabricantes = ['Mercedes', 'Audi', 'BMW'];
 
-function imprimir(nome, indice) {
-    console.log(`${indice + 1}. ${nome}`);
+// function imprimir(nome, indice) {
+//     console.log(`${indice + 1}. ${nome}`);
+// }
+// fabricantes.forEach(imprimir);
+
+// fabricantes.forEach(function(a) {
+//     console.log(a);
+// })
+
+const notas = [7, 8, 9, 5, 6];
+
+// Sem callback
+const notasBaixas = [];
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas.push(notas[i])
+    }
 }
-fabricantes.forEach(imprimir);
 
-fabricantes.forEach(function(a) {
-    console.log(a);
-})
+console.log(notasBaixas);
+
+// Com callback
+const notasBaixas2 = notas.filter(function(nota) {
+    return nota < 7;
+});
+
+console.log(notasBaixas2);
+
+const notasBaixas3 = notas.filter((nota) => nota < 7);
+
+console.log(notasBaixas3)
