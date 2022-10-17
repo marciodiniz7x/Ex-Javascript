5eb1704b5554eabe77ccd7e5d936c1b3ca70a16f
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-17 13:50:59 -0300

    Update ex005-Closure.js

--- ex005-Closure.js
+++ ex005-Closure.js
@@ -1,2 +1,14 @@
 // Closure: escopo criado quando uma função é declarada.
 
+const x = 'Global';
+
+function fora() {
+    const x = 'Local';
+    function dentro() {
+        return x;
+    }
+    return dentro
+}
+
+const minhaFuncao = fora();
+console.log(minhaFuncao());
